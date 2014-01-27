26d3d3b2fcf60768879f4ebcfa9c84072a0536b8
evdokim <ekovach@ohnosequences.com>
2014-01-27 18:54:38 +0100

    import gi to tax mapping

--- src/main/scala/ohnosequences/metapasta/Metapasta.scala
+++ src/main/scala/ohnosequences/metapasta/Metapasta.scala
@@ -7,7 +7,26 @@ import ohnosequences.awstools.s3.ObjectAddress
 import ohnosequences.awstools.ec2.InstanceType
 import ohnosequences.awstools.autoscaling.OnDemand
 import ohnosequences.nisperon.queues.{unitQueue, ProductQueue}
-import com.amazonaws.services.dynamodbv2.model.{AttributeValue, Condition, QueryRequest, ScanRequest, ComparisonOperator}
+import com.amazonaws.services.dynamodbv2.model._
+import java.io.{PrintWriter, File}
+import scala.collection.mutable
+import ohnosequences.nisperon.Group
+import ohnosequences.metapasta.MergedSampleChunk
+import ohnosequences.nisperon.NisperonConfiguration
+import ohnosequences.nisperon.NisperoConfiguration
+import ohnosequences.metapasta.PairedSample
+import ohnosequences.awstools.s3.ObjectAddress
+import ohnosequences.nisperon.queues.ProductQueue
+import ohnosequences.metapasta.AssignTable
+import ohnosequences.nisperon.Group
+import ohnosequences.metapasta.MergedSampleChunk
+import ohnosequences.nisperon.NisperonConfiguration
+import ohnosequences.nisperon.NisperoConfiguration
+import ohnosequences.metapasta.PairedSample
+import ohnosequences.awstools.s3.ObjectAddress
+import ohnosequences.nisperon.queues.ProductQueue
+import ohnosequences.metapasta.AssignTable
+import java.util
 
 
 object Metapasta extends Nisperon {
@@ -60,7 +79,7 @@ object Metapasta extends Nisperon {
     inputQueue = mergedSampleChunks,
     outputQueue = ProductQueue(readsInfo, assignTable),
     instructions = new LastInstructions(aws, new NTLastDatabase(aws)),
-    nisperoConfiguration = NisperoConfiguration(nisperonConfiguration, "last", workerGroup = Group(size = 2, max = 15, instanceType = InstanceType.M1Medium, purchaseModel = OnDemand))
+    nisperoConfiguration = NisperoConfiguration(nisperonConfiguration, "last", workerGroup = Group(size = 4, max = 15, instanceType = InstanceType.M1Medium, purchaseModel = OnDemand))
   )
 
   val uploaderNispero = nispero(
@@ -72,6 +91,8 @@ object Metapasta extends Nisperon {
 
 
   def undeployActions() {
+
+    //create csv
 
     println("undeploy!")
   }
@@ -110,7 +131,6 @@ object Metapasta extends Nisperon {
           val t  = reads.size
 
 
-
           a += n
           b += t
           println("n: " + n)
@@ -123,6 +143,139 @@ object Metapasta extends Nisperon {
 
     println("unassigned:  " + a)
     println("total:  " + b)
+  }
+
+  def ntgi() {
+    val ntFile = new File("nt.fasta")
+    val s = scala.io.Source.fromFile(ntFile)
+    val l = new mutable.HashMap[String, String]()
+    val giP = """>gi\|(\d+)\|.+""".r
+    logger.info("processing " + ntFile.getPath)
+    var counter = 0
+    for(line <- s.getLines()) {
+      counter += 1
+      if(counter % 10000 ==0) {
+        logger.info("processed " + counter)
+      }
+      line match {
+        case giP(gi) => l.put(gi, "")
+        case ss if ss.startsWith(">") => logger.error(ss)
+        case _ =>
+      }
+    }
+    logger.info("finished processing " + ntFile.getPath)
+    logger.info("parsed " + l.size + " items")
+
+    val giFile = new File("gi.dmp")
+    val taxP = """\s*(\d+)\s+(\d+)\s*""".r
+    val taxSource = scala.io.Source.fromFile(giFile)
+    logger.info("parsing " + giFile.getPath)
+    counter = 0
+    val gimap = new PrintWriter(new File("gi.map"))
+    val undef = new PrintWriter(new File("gi.undef"))
+    for(line <- taxSource.getLines()) {
+      counter += 1
+      if(counter % 100000 ==0) {
+        logger.info("processed " + counter)
+      }
+      line match {
+        case taxP(gi, taxId) => {
+          if(l.contains(gi)) {
+            l.put(gi, taxId)
+            gimap.println(gi + " " + taxId)
+          }
+        }
+        case unparsed => logger.error(unparsed)
+      }
+    }
+
+    l.toList.foreach { case (gi, tax) =>
+      if(tax.isEmpty) {
+        undef.println(gi)
+      }
+    }
+    undef.close()
+    gimap.close()
+
+
+
+
+
+
+    logger.info("finished processing " + giFile.getPath)
+//    counter = 0
+//    l.toList.grouped(25).foreach { chunk =>
+//      counter += 1
+//      logger.info("chunk " + counter)
+//      val writeOperations = new java.util.ArrayList[WriteRequest]()
+//      chunk.foreach { case (gi, tax) =>
+//        val item = new util.HashMap[String, AttributeValue]()
+//        item.put("gi" , new AttributeValue().withS(gi))
+//        item.put("tax" , new AttributeValue().withS(tax))
+//
+//        if (!tax.isEmpty) {
+//        writeOperations.add(new WriteRequest()
+//          .withPutRequest(new PutRequest()
+//          .withItem(item)
+//          ))
+//        } else {
+//          logger.error(gi)
+//        }
+//      }
+//
+//      if (!writeOperations.isEmpty) {
+//
+//        var operations: java.util.Map[String, java.util.List[WriteRequest]] = new java.util.HashMap[String, java.util.List[WriteRequest]]()
+//        operations.put("nt_gi_index", writeOperations)
+//        do {
+//          //to
+//          try {
+//            val res = aws.ddb.batchWriteItem(new BatchWriteItemRequest()
+//              .withRequestItems(operations)            )
+//            operations = res.getUnprocessedItems
+//           // val size = operations.values().map(_.size()).sum
+//           // logger.info("unprocessed: " + size)
+//          } catch {
+//            case t: ProvisionedThroughputExceededException => logger.warn(t.toString + " " + t.getMessage)
+//          }
+//        } while (!operations.isEmpty)
+//      }
+//    }
+
+
+   // println(l.take(10).toList)
+
+  }
+
+  def additionalHandler(args: List[String]) {
+    ntgi()
+//    logger.info("additional" + args)
+//    args match {
+//    case "bio4j" :: Nil => {
+//      import ohnosequences.bio4j.distributions._
+//      logger.info("installing bio4j")
+//      println(Bio4jDistributionDist2.installWithDeps(Bio4jDistribution.GITaxonomyIndex))
+//      logger.info("getting database connection")
+//      val nodeRetriver =Bio4jDistribution.GITaxonomyIndex.nodeRetriever
+//      val gi = "23953857"
+//                  logger.info("receiving node " + gi)
+//                  val node = nodeRetriver.getNCBITaxonByGiId(gi)
+//
+//                  if (node != null) {
+//                    println("name: " + node.getName())
+//                    println("taxid: " + node.getTaxId())
+//                    println("rank: " + node.getRank())
+//
+//                    val parent = node.getParent()
+//                    println("name: " + parent.getName())
+//                    println("taxid: " + parent.getTaxId())
+//                    println("rank: " + parent.getRank())
+//                  } else {
+//                    logger.info("received null")
+//                  }
+//    }
+//  }
+
   }
 
   def addTasks() {
