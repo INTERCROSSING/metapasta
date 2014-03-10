817be3a6c864f1ac74479f0cca3b19aa6c7c759c
evdokim <ekovach@ohnosequences.com>
2014-03-10 19:37:53 +0100

    updated nt database

--- src/main/scala/ohnosequences/metapasta/NTDatabase.scala
+++ src/main/scala/ohnosequences/metapasta/NTDatabase.scala
@@ -11,18 +11,14 @@ object NTDatabase extends BlastDatabase {
   val logger = Logger(this.getClass)
 
   def install(aws: AWS) {
-    import scala.sys.process._
-
     logger.info("downloading database")
     val lm = aws.s3.createLoadingManager()
-    lm.download(ObjectAddress("metapasta", "nt.blast.zip"), new File("database.zip"))
 
-    logger.info("extracting database")
-    """unzip database.zip""".!
+    lm.downloadDirectory(ObjectAddress("metapasta", "nt.march.14"), new File("."))
 
   }
 
-  val name: String = "nt.16S.fasta"
+  val name: String = "nt.march.14/nt.march.14.fasta"
 
   //gi|313494140|gb|GU939576.1|
   val re = """\Qgi|\E(\d+)[^\d]+.*""".r
