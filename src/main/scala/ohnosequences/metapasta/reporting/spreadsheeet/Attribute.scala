9aa17af70410cbadcec82b0d2f5cc8ddc96b33d0
evdokim <ekovach@ohnosequences.com>
2014-07-02 18:49:10 +0200

    basic combinators for attributes

--- src/main/scala/ohnosequences/metapasta/reporting/spreadsheeet/Attribute.scala
+++ src/main/scala/ohnosequences/metapasta/reporting/spreadsheeet/Attribute.scala
@@ -2,128 +2,48 @@ package ohnosequences.metapasta.reporting.spreadsheeet
 
 import ohnosequences.nisperon.{doubleMonoid, intMonoid, Monoid}
 import scala.collection.mutable
+import ohnosequences.metapasta.reporting.StringConstantMonoid
 
-trait AnyAttributeValue {
-  type Type
-  val value: Type
-}
-
-object AnyAttributeValue {
-  type Of[T] = AnyAttributeValue { type Type = T }
-}
-
-class IntAttributeValue(val value: Int) extends AnyAttributeValue {
-  type Type = Int
-
-  override def toString: String = value.toString
-}
-
-class DoubleAttributeValue(val value: Double) extends AnyAttributeValue {
-  type Type = Double
-
-  override def toString: String = value.toString
-}
 
 trait Context {
-  def get[A <: AnyAttribute](attribute: A, index: Int): Option[attribute.Value]
-  def getTotal[A <: AnyAttribute](attribute: A): attribute.Value
-  def set[A <: AnyAttribute](attribute: A, index: Int)(value: attribute.Value)
- // def setTotal[A <: AnyAttribute](attribute: A)(value: attribute.Value)
+  def get[A <: AnyAttribute](attribute: A, index: Int): attribute.Type
+  def getTotal[A <: AnyAttribute](attribute: A): attribute.Type
+  def set[A <: AnyAttribute](attribute: A, index: Int)(value: attribute.Type)
 }
 
 class ListContext(val attributes: List[AnyAttribute]) extends Context {
 
-  val map = new mutable.HashMap[(String, Int), AnyAttributeValue]()
+  val map = new mutable.HashMap[(String, Int), Any]()
 
-  val totals = new mutable.HashMap[String, AnyAttributeValue]()
-
-
-
-//  override def setTotal[A <: AnyAttribute](attribute: A)(value: attribute.Value): Unit = {
-//
-//  }
+  val totals = new mutable.HashMap[String, Any]()
 
   for (attribute <- attributes) {
-    totals.put(attribute.name, attribute.wrap(attribute.monoid.unit))
+    totals.put(attribute.name, attribute.monoid.unit)
   }
 
-  override def set[A <: AnyAttribute](attribute: A, index: Int)(value: attribute.Value): Unit = {
-    val cur = totals.get(attribute.name) match {
-      case None => attribute.monoid.unit
-      case Some(va) => {
-        va.asInstanceOf[attribute.Value].value
-      }
-    }
-    totals.put(attribute.name, attribute.wrap(attribute.monoid.mult(cur, value.value)))
+  override def set[A <: AnyAttribute](attribute: A, index: Int)(value: attribute.Type): Unit = {
+    val cur = totals.getOrElse(attribute.name, attribute.monoid.unit).asInstanceOf[attribute.Type]
+    totals.put(attribute.name, attribute.monoid.mult(cur, value))
     map.put((attribute.name, index), value)
   }
 
-  override def getTotal[A <: AnyAttribute](attribute: A): attribute.Value = {
-    attribute.wrap(totals.get(attribute.name) match {
-      case None => attribute.monoid.unit
-      case Some(va) => {
-        va.asInstanceOf[attribute.Value].value
-      }
-    })
+  override def getTotal[A <: AnyAttribute](attribute: A): attribute.Type = {
+    totals.getOrElse(attribute.name, attribute.monoid.unit).asInstanceOf[attribute.Type]
   }
 
-  override def get[A <: AnyAttribute](attribute: A, index: Int): Option[attribute.Value] = {
-    map.get((attribute.name, index)).map(_.asInstanceOf[attribute.Value])
+  override def get[A <: AnyAttribute](attribute: A, index: Int): attribute.Type = {
+    map.getOrElse((attribute.name, index), attribute.monoid.unit).asInstanceOf[attribute.Type]
   }
 }
-
-
 
 trait AnyAttribute {
   type Type
 
-  type Value = AnyAttributeValue.Of[Type]
   val monoid: Monoid[Type]
   val name: String
-
   type Item
 
-//  def apply(item: Item, index: Int, context: Context): Value = {
-//    context.get(this, index) match {
-//      case None => {
-//        val newVal: Value = execute(item, index, context)
-//        context.set(this, index)(newVal)
-//        newVal
-//      }
-//      case Some(value) => {
-//        value
-//      }
-//    }
-//  }
-
-  def apply(context: Context): Value = {
-    context.getTotal(this)
-//    context.getTotal(this) match {
-//      case None => {
-//        //calculate total:
-//        println("wrong refer")
-//        wrap(monoid.unit)
-//      }
-//      case Some(va) => va
-//    }
-  }
-
-  def apply(item: Item, index: Int, context: Context): Value = {
-    context.get(this, index) match {
-      case None => {
-        val newVal: Value = execute(item, index, context)
-        context.set(this, index)(newVal)
-        newVal
-      }
-      case Some(value) => {
-        value
-      }
-    }
-  }
-
-  def wrap(value: Type): Value
-
-  def execute(item: Item, index: Int, context: Context): Value
+  def execute(item: Item, index: Int, context: Context): Type
 
 }
 
@@ -131,68 +51,66 @@ object AnyAttribute {
   type For[T] = AnyAttribute { type Item = T }
 }
 
-trait IntAttribute extends AnyAttribute {
+abstract class IntAttribute[I](val name: String, val monoid: Monoid[Int]) extends AnyAttribute {
   override type Type = Int
+  override type Item = I
+}
 
-  override def wrap(value: Type): Value = {
-    new IntAttributeValue(value)
+abstract class DoubleAttribute[I](val name: String, val monoid: Monoid[Double]) extends AnyAttribute {
+  override type Type = Double
+  override type Item = I
+}
+
+abstract class StringAttribute[I](val name: String, val monoid: Monoid[String]) extends AnyAttribute {
+  override type Type = String
+  override type Item = I
+}
+
+
+
+case class Freq[I](a: IntAttribute[I]) extends DoubleAttribute[I](a.name + ".freq", doubleMonoid) {
+  override def execute(item: Item, index: Int, context: Context) = {
+    context.get(a, index).toDouble / context.getTotal(a)
   }
 }
 
-trait DoubleAttribute extends AnyAttribute {
-  override type Type = Double
-
-  override def wrap(value: Type): Value = {
-    new DoubleAttributeValue(value)
+case class Sum[I](a: IntAttribute[I]*) extends IntAttribute[I](a.map(_.name).reduce { _ + "+" + _}, intMonoid) {
+  override def execute(item: Item, index: Int, context: Context) = {
+    a.map {context.get(_, index)}.reduce{_ + _}
   }
 }
 
 object Test {
-  object Id extends IntAttribute {
-    type Item = (Int, String)
-    val name: String = "id"
-    val monoid: Monoid[Int] = intMonoid
-    def execute(item: Item, index: Int, context: Context): Value = {
-      wrap(item._1)
+  object Id extends StringAttribute[(Int, String)]("id", new StringConstantMonoid("total")) {
+    override type Item = (Int, String)
+    override  def execute(item: Item, index: Int, context: Context) = {
+      item._1.toString
     }
   }
 
-  object Counter extends IntAttribute {
-    type Item = (Int, String)
-    val name: String = "counter"
-    val monoid: Monoid[Int] = intMonoid
-    def execute(item: Item, index: Int, context: Context): Value = {
-      val prev: Int = if (index < 1) {
-        0
-      } else {
-        context.get(Counter, index -1).map(_.value).getOrElse(0)
-      }
-      wrap(prev + 1)
+  object Name extends StringAttribute[(Int, String)]("name", new StringConstantMonoid("")) {
+    override type Item = (Int, String)
+    override  def execute(item: Item, index: Int, context: Context) = {
+      item._2
     }
   }
 
-  object Freq extends DoubleAttribute {
-    type Item = (Int, String)
-    val name: String = "freq"
-    val monoid = doubleMonoid
-    def execute(item: Item, index: Int, context: Context): Value = {
-      val total: Int = Counter.apply(context).value
-
-      wrap((Counter.apply(item, index, context).value + 0.0) / total)
+  object Counter extends IntAttribute[(Int, String)]("counter", intMonoid) {
+    override def execute(item: Item, index: Int, context: Context) = {
+      context.get(Counter, index -1) + 1
     }
   }
 
 
   def main(args: Array[String]) {
     val items = List (
-      (1, "one"), (2, "two"), (3, "thee")
+      (1, "one"), (2, "two"), (3, "three"), (123, "one-two-three")
     )
 
-    val attributes = List(Id, Counter, Freq)
+    val attributes = List(Id, Name, Counter, Sum(Counter, Counter), Freq(Counter))
     val executor = new Executor(attributes, items)
     executor.execute()
 
-   // Counter.
   }
 }
 
@@ -204,7 +122,8 @@ class Executor[Item](attributes: List[AnyAttribute.For[Item]], items: Iterable[I
     for (attribute <- attributes) {
       var index = 0
       for (item <- items) {
-        val res = attribute.apply(item, index, context)
+        val res = attribute.execute(item, index, context)
+        context.set(attribute, index)(res)
         println(attribute.name + "[" + index + "] = " + res)
         index += 1
       }
