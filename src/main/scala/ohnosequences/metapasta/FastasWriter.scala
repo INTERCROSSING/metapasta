a38ab0238c632e6316b745a289d9a644c66883e4
evdokim <ekovach@ohnosequences.com>
2014-08-14 20:04:42 +0100

    introduced assignments categories. to be continued..

---
+++ src/main/scala/ohnosequences/metapasta/FastasWriter.scala
@@ -0,0 +1,59 @@
+package ohnosequences.metapasta
+
+import ohnosequences.formats.{RawHeader, FASTQ}
+import scala.collection.mutable
+import ohnosequences.nisperon.logging.S3Logger
+import ohnosequences.nisperon.AWS
+import ohnosequences.awstools.s3.ObjectAddress
+
+class FastasWriter(s3logger: S3Logger, logging: Boolean) {
+  val noHitFasta = new mutable.StringBuilder()
+  val noTaxIdFasta = new mutable.StringBuilder()
+  val notAssignedFasta = new mutable.StringBuilder()
+  val assignedFasta = new mutable.StringBuilder()
+
+  def fastaHeader(sampleId: String, taxId: String, refIds: List[String]): String = {
+
+    val (taxname, rank) = try {
+      val node = nodeRetriever.nodeRetriever.getNCBITaxonByTaxId(taxId)
+      (node.getScientificName(), node.getRank())
+    } catch {
+      case t: Throwable => ("na", "na")
+    }
+    sampleId + "|" + taxname + "|" + taxId + "|" + rank + "|" + refIds.foldRight("")(_ + "|" + _)
+  }
+
+  def write(chunk: MergedSampleChunk, read: FASTQ[RawHeader], readId: String, assignment: Assignment) {
+    assignment match {
+      case TaxIdAssignment(taxId, refIds) => {
+        assignedFasta.append(read.toFasta(fastaHeader(chunk.sample, taxId, refIds)))
+        assignedFasta.append(System.lineSeparator())
+      }
+      case NoTaxIdAssignment(refId) => {
+        noTaxIdFasta.append(read.toFasta)
+        noTaxIdFasta.append(System.lineSeparator())
+      }
+      case unknown => {
+        s3logger.warn("fastasWriter: unknown assignment: " + unknown)
+      }
+    }
+  }
+
+  //hohit
+  def writeNoHit(read: FASTQ[RawHeader], readId: String) {
+    noHitFasta.append(read.toFasta)
+    noHitFasta.append(System.lineSeparator())
+
+  }
+
+  def uploadFastas(aws: AWS, readsDirectory: ObjectAddress, chunk: MergedSampleChunk, assignmentType: AssignmentType) {
+    if (logging) {
+      // upload fastas
+      aws.s3.putWholeObject(S3Paths.noHitFasta(readsDirectory, chunk), noHitFasta.toString())
+      aws.s3.putWholeObject(S3Paths.noTaxIdFasta(readsDirectory, chunk), noTaxIdFasta.toString())
+      aws.s3.putWholeObject(S3Paths.notAssignedFasta(readsDirectory, chunk, assignmentType), notAssignedFasta.toString())
+      aws.s3.putWholeObject(S3Paths.assignedFasta(readsDirectory, chunk, assignmentType), assignedFasta.toString())
+    }
+  }
+
+}
